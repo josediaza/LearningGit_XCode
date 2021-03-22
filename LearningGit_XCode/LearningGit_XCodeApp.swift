42a6da4d5d27d58ff29db73452fb38ecf7d237f7
Jose Diaz Arrechea <josediaz.arrechea@gmail.com>
2021-03-21 20:27:22 -0400

    Initial Commit

---
+++ LearningGit_XCode/LearningGit_XCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGit_XCodeApp.swift
+//  LearningGit_XCode
+//
+//  Created by Jose Diaz Arrechea on 2021-03-21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGit_XCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
